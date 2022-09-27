622d5961075053fc5054f7cc8f2e351c2d7f9a82
James Salinas <76701116+jsalinas214@users.noreply.github.com>
2022-09-27 10:02:50 -0400

    Create functions.js
    Created functions.js to complete the coding challenges for Functions in Eloquent JS.

---
+++ functions.js
@@ -0,0 +1,30 @@
+/*
+  Minimum
+  
+  Write a function min that takes 2 arguments and returns their minimum.
+  
+  
+*/
+
+/*
+  Recursion
+  
+  -- Zero is even.
+  -- One is odd.
+  -- Foi any other number N, its eveness is the same as N - 2.
+  
+  Define a recursive function isEven corresponding to this description.
+  the function should accept a single parameter (a positive, whole number) and returns a boolean.
+  Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+*/
+
+/*
+  Bean Counting
+  
+  Write a fucntion countBs that takes a string as its only argument and 
+  returns a number that indicates how many uppercase "B" characters there are in the string.
+  
+  Next, write a funciton called countChar that behaves like countBs, except it takes a second argument that indicstes the character that is to be counted.
+  
+  Rewrite countBs to make use of this new function.
+*/
